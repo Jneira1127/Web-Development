1250134ff8f225f283af4df8550e46d3700eb836
JasonNeira <100978784+Jneira1127@users.noreply.github.com>
2025-08-08 09:07:45 -0500

    Update QuotesNExpalin.js

--- Projects/Inspiration Website/assets/QuotesNExpalin.js
+++ Projects/Inspiration Website/assets/QuotesNExpalin.js
@@ -1,12 +1,12 @@
 const inspire = [
-  quote1 = {
-    quote = "something to me"
-    explain = "this  is meant for me"
-    author = "Jason N."
+  {
+    quote: "something to me",
+    explain: "this  is meant for me",
+    author: "Jason N."
   },
-  quote2 = {
-    quote = "Do or not do, there is no try"
-    explain = "You either do it or not"
-    author = "Master Yoda"
+  {
+    quote:"Do or not do, there is no try",
+    explain:"You either do it or not",
+    author:"Master Yoda"
   }
 ]
