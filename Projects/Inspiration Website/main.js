1c88bc3b0f4b580657837d7895193d81b9f384a3
JasonNeira <100978784+Jneira1127@users.noreply.github.com>
2025-08-12 12:41:41 -0500

    Update main.js

--- Projects/Inspiration Website/main.js
+++ Projects/Inspiration Website/main.js
@@ -1,1 +1,12 @@
+//local and global variables
+const randomIND= Math.floor(Math.random * quotes.length)
+
+//functions and events
+const printQuote = () => {
+  return quotes[randomIND]
+}
+
+const printExplanation = () => {
+  return explain[randomIND]
+}
 
